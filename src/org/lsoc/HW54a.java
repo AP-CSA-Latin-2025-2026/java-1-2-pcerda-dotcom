6e7125204c29278b8bce6c1296f42799569ff519
Paloma <pcerda@lsoc.org>
2025-09-04 17:32:00 +0000

    54a

---
+++ src/org/lsoc/HW54a.java
@@ -0,0 +1,12 @@
+public class HW54a {
+  public static void main(String[] args) {
+    double bug = 286/9;
+    double fire = 412/40;
+    double sub = 361/18;
+    double cut = 161/11;
+    System.out.println(bug);
+    System.out.println(fire);
+    System.out.println(sub);
+    System.out.println(cut);
+  }
+}
