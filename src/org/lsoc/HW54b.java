e0778ec2971eb716c47e5c3e9b8f794e00c0d306
Paloma <pcerda@lsoc.org>
2025-09-04 21:21:51 +0000

    54b

---
+++ src/org/lsoc/HW54b.java
@@ -0,0 +1,13 @@
+public class HW54b {
+  public static void main(String[] args) {
+    int one = 475;
+    int two = 821;
+    int three = 369;
+    int four = 562;
+    int sum = one + two + three + four;
+    int avg = one + two + three + four/4;
+    System.out.println("The sum of the four numbers is " + sum);
+    System.out.println();
+    System.out.println("The average of the four numbers is " + avg);
+  }
+}
